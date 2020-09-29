5607ecc069f8c933e6989f2030da2d872d921428
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2020-09-29 17:26:19 -0400

    init

---
+++ webhook.py
@@ -0,0 +1,45 @@
+import requests
+from stringcase import titlecase
+from secret import webhook_url
+
+def send(title, desc, status, workType, points=0, url=None):
+    workType = titlecase(workType.lower())
+    status = titlecase(status.lower())
+    url = webhook_url
+
+    embed = {
+        "color": 3447003,
+        "title": status + " - " + workType + " " + title,
+        "description": workType
+        + " details: "
+        + ((desc[:75] + "..") if len(desc) > 75 else desc),
+        "fields": [
+            {
+                "name": "Points",
+                "value": points,
+            },
+        ],
+    }
+    if url:
+        embed["url"] = url
+
+    data = {
+        "embeds": [embed],
+        "username": "Google Classroom Bot",
+        "avatar_url": "https://lh3.googleusercontent.com/jdcCuHVB2NoCEdDqj1fNV05G8MC3TyBX6jY93v_Sba2ViqrVXIW-efKjVk3BR-41VhwV8gD0x0EHmlXK2UqvCCQLDTqOs2N1AXjppA=w1440-v1",
+    }
+
+    print(requests.post(url, json=data).text)
+
+
+def received_stuff(data_object, status):
+    d = data_object
+
+    send(
+        title=d["title"],
+        desc=d["description"],
+        status=status,
+        points=int(d["maxPoints"]),
+        workType=d["workType"],
+        url=d["alternateLink"],
+    )
