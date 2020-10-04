3683fe2f45f78470c78e057f93a3132bdf7a2499
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2020-10-03 21:55:17 -0400

    https

--- fe.py
+++ fe.py
@@ -43,16 +43,19 @@ def root():
 @login_required
 def logout():
     logout_user()
-    return redirect("/")
+    return httpsit(request.host_url) + '/'
+
 
 def httpsit(st):
-    return 'https://'+st.split('://')[1]
+    return "https://" + st.split("://")[1]
+
+
 @app.route("/login")
 def login():
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         "credentials_new.json", scopes=scopes
     )
-    flow.redirect_uri = httpsit(request.base_url)+ "/callback"
+    flow.redirect_uri = httpsit(request.base_url) + "/callback"
     authorization_url, state = flow.authorization_url(
         access_type="offline",
     )
@@ -75,7 +78,7 @@ def callback():
 
     user = add_or_update_user(credentials, db)
     login_user(user)
-    return redirect('/edit')
+    return httpsit(request.host_url) + "/edit"
 
 
 @app.route("/edit", methods=["GET", "POST"])
@@ -109,7 +112,7 @@ def edit_page():
                     uid=uid, classId=c["id"], webhook=request.form["url-" + idd], db=db
                 )
 
-        return redirect('/edit')
+        return httpsit(request.host_url) + "/edit"
 
     return render_template("edit.html", classes=classes)
 
